994ac29978ddd131a81194190f0db894a6bfb6c0
James Coffman <jamesc4023@gmail.com>
2022-06-08 15:56:59 -0400

    commit tree e860ceb5d259fe0f48b3ed8760cfff5801966950

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_res
+
+  private
+
+  def not_found_res invalid
+
+    #type = self.class.to_s.gsub('Controller', '')
+
+    #render json: { errors: "#{type} not found" }, status: :not_found
+    
+    render json: { errors: "#{invalid.model} not found" }, status: :not_found
+
+  end
+
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,40 @@
 class ItemsController < ApplicationController
 
+  
   def index
-    items = Item.all
+
+    if params[:user_id]
+      items = find_user_items
+    else
+      items = Item.all
+    end
+
     render json: items, include: :user
+
+  end
+
+  def show
+    render json: find_item, status: :found 
+  end
+
+  def create
+    user_items = find_user_items
+    user_items.create(item_params)
+    render json: user_items.last, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def find_item
+    Item.find(params[:id])
+  end
+
+  def find_user_items
+    User.find(params[:user_id]).items
   end
 
 end
